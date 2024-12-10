f278c3dbc6afa7cd99816a873b780d56125886c8
Nick Braun <nabraun03@gmail.com>
2024-12-10 17:37:16 -0500

    Last week's updates

--- preproccessing_new.py
+++ preproccessing_new.py
@@ -59,93 +59,196 @@ class Preprocessor:
                 ]
             ]
 
-        df_traditional = pd.read_csv(f"{season}_traditional_stats.csv")[
-            [
-                    "gameId",
-                    "teamTricode",
-                    "fieldGoalsMade",
-                    "fieldGoalsAttempted",
-                    "fieldGoalsPercentage",
-                    "threePointersMade",
-                    "threePointersAttempted",
-                    "threePointersPercentage",
-                    "freeThrowsMade",
-                    "freeThrowsAttempted",
-                    "freeThrowsPercentage",
-                    "reboundsOffensive",
-                    "reboundsDefensive",
-                    "reboundsTotal",
-                    "assists",
-                    "steals",
-                    "blocks",
-                    "turnovers",
-                    "foulsPersonal",
-                    "points",
-                    "plusMinusPoints",
+            df_traditional = pd.read_csv(f"{season}_traditional_stats.csv")[
+                [
+                        "gameId",
+                        "teamTricode",
+                        "fieldGoalsMade",
+                        "fieldGoalsAttempted",
+                        "fieldGoalsPercentage",
+                        "threePointersMade",
+                        "threePointersAttempted",
+                        "threePointersPercentage",
+                        "freeThrowsMade",
+                        "freeThrowsAttempted",
+                        "freeThrowsPercentage",
+                        "reboundsOffensive",
+                        "reboundsDefensive",
+                        "reboundsTotal",
+                        "assists",
+                        "steals",
+                        "blocks",
+                        "turnovers",
+                        "foulsPersonal",
+                        "points",
+                        "plusMinusPoints",
+                ]
             ]
-        ]
 
-        df_hustle = pd.read_csv(f"{season}_hustle_stats.csv")[
+            df_hustle = pd.read_csv(f"{season}_hustle_stats.csv")[
+                    [
+                        "gameId",
+                        "teamTricode",
+                        "contestedShots",
+                        "contestedShots2pt",
+                        "contestedShots3pt",
+                        "deflections",
+                        "chargesDrawn",
+                        "screenAssists",
+                        "screenAssistPoints",
+                        "looseBallsRecoveredOffensive",
+                        "looseBallsRecoveredDefensive",
+                        "looseBallsRecoveredTotal",
+                        "offensiveBoxOuts",
+                        "defensiveBoxOuts",
+                        "boxOutPlayerTeamRebounds",
+                        "boxOutPlayerRebounds",
+                        "boxOuts",
+                    ]
+                ]
+            df_misc = pd.read_csv(f"{season}_misc_stats.csv")[
                 [
                     "gameId",
                     "teamTricode",
-                    "contestedShots",
-                    "contestedShots2pt",
-                    "contestedShots3pt",
-                    "deflections",
-                    "chargesDrawn",
-                    "screenAssists",
-                    "screenAssistPoints",
-                    "looseBallsRecoveredOffensive",
-                    "looseBallsRecoveredDefensive",
-                    "looseBallsRecoveredTotal",
-                    "offensiveBoxOuts",
-                    "defensiveBoxOuts",
-                    "boxOutPlayerTeamRebounds",
-                    "boxOutPlayerRebounds",
-                    "boxOuts",
+                    "pointsOffTurnovers",
+                    "pointsSecondChance",
+                    "pointsFastBreak",
+                    "pointsPaint",
+                    "oppPointsOffTurnovers",
+                    "oppPointsSecondChance",
+                    "oppPointsFastBreak",
+                    "oppPointsPaint",
+                    "blocksAgainst",
+                    "foulsDrawn",
                 ]
             ]
-        df_misc = pd.read_csv(f"{season}_misc_stats.csv")[
-            [
-                "gameId",
-                "teamTricode",
-                "pointsOffTurnovers",
-                "pointsSecondChance",
-                "pointsFastBreak",
-                "pointsPaint",
-                "oppPointsOffTurnovers",
-                "oppPointsSecondChance",
-                "oppPointsFastBreak",
-                "oppPointsPaint",
-                "blocksAgainst",
-                "foulsDrawn",
+            df_tracking = pd.read_csv(f"{season}_track_stats.csv")[
+                [
+                    "gameId",
+                    "teamTricode",
+                    "distance",
+                    "reboundChancesOffensive",
+                    "reboundChancesDefensive",
+                    "reboundChancesTotal",
+                    "touches",
+                    "secondaryAssists",
+                    "freeThrowAssists",
+                    "passes",
+                    "contestedFieldGoalsMade",
+                    "contestedFieldGoalsAttempted",
+                    "contestedFieldGoalPercentage",
+                    "uncontestedFieldGoalsMade",
+                    "uncontestedFieldGoalsAttempted",
+                    "uncontestedFieldGoalsPercentage",
+                    "defendedAtRimFieldGoalsMade",
+                    "defendedAtRimFieldGoalsAttempted",
+                    "defendedAtRimFieldGoalPercentage",
+                ]
             ]
-        ]
-        df_tracking = pd.read_csv(f"{season}_track_stats.csv")[
-            [
-                "gameId",
-                "teamTricode",
-                "distance",
-                "reboundChancesOffensive",
-                "reboundChancesDefensive",
-                "reboundChancesTotal",
-                "touches",
-                "secondaryAssists",
-                "freeThrowAssists",
-                "passes",
-                "contestedFieldGoalsMade",
-                "contestedFieldGoalsAttempted",
-                "contestedFieldGoalPercentage",
-                "uncontestedFieldGoalsMade",
-                "uncontestedFieldGoalsAttempted",
-                "uncontestedFieldGoalsPercentage",
-                "defendedAtRimFieldGoalsMade",
-                "defendedAtRimFieldGoalsAttempted",
-                "defendedAtRimFieldGoalPercentage",
-            ]
+
+            merge_columns = ["gameId", "teamTricode"]
+            merged_df = pd.merge(df_advanced, df_traditional, on=merge_columns, how="inner")
+            merged_df = pd.merge(merged_df, df_hustle, on=merge_columns, how="inner")
+            merged_df = pd.merge(merged_df, df_misc, on=merge_columns, how="inner")
+            merged_df = pd.merge(merged_df, df_tracking, on=merge_columns, how="inner")
+            merged_df = merged_df.drop_duplicates()
+
+            merged_df = pd.merge(
+                merged_df, df_games[["gameId", "GAME_DATE"]], on="gameId", how="inner"
+            )
+
+            merged_df["date"] = merged_df["GAME_DATE"].apply(
+                lambda x: date(*map(int, x.split("-")))
+            )
+
+            merged_df = merged_df.drop(columns = ["GAME_DATE"])
+
+            df_games["winner"] = np.where(
+                df_games["HOME_TEAM_PTS"] > df_games["AWAY_TEAM_PTS"],
+                df_games["HOME_TEAM_ABBREVIATION"],
+                df_games["AWAY_TEAM_ABBREVIATION"]
+            )
+
+            merged_df = pd.merge(
+                merged_df,
+                df_games[
+                    [
+                    "gameId",
+                    "winner",
+                    "HOME_TEAM_ABBREVIATION",
+                    "AWAY_TEAM_ABBREVIATION"
+                ]],
+                on="gameId",
+                how="inner"
+            )
+
+            processed_df = self.preprocess_team_data(merged_df)
+
+            seasons.append(processed_df)
+
+        self.team_stats = pd.concat(seasons)
+
+    def preprocess_team_data(self, df):
+
+        grouped = df.groupby("teamTricode")
+        modified_groups = []
+        for name, group in grouped:
+            group["game_count"] = (
+                group["gameId"].expanding().count().shift(self.shift).fillna(0)
+            )
+            group["time_between_games"] = group["date"].diff().dt.days
+            group["playoff"] = (group["game_count"] > 82).astype(int)
+            group = self.generate_team_running_average(group)
+
+            modified_groups.append(group)
+        return pd.concat(modified_groups)
+    
+    def generate_team_running_average(self, group):
+        percentage_columns = [
+            "assistPercentage",
+            "assistToTurnover",
+            "assistRatio",
+            "offensiveReboundPercentage",
+            "defensiveReboundPercentage",
+            "reboundPercentage",
+            "turnoverRatio",
+            "effectiveFieldGoalPercentage",
+            "trueShootingPercentage",
+            "usagePercentage",
+            "estimatedUsagePercentage",
+            "fieldGoalsPercentage",
+            "threePointersPercentage",
+            "freeThrowsPercentage",
+            "contestedFieldGoalPercentage",
+            "uncontestedFieldGoalsPercentage",
+            "defendedAtRimFieldGoalPercentage",
         ]
 
+        averaging_columns = [
+            col for col in group.columns if (col not in [
+                "teamTricode",
+                "gameId",
+                "date",
+                "game_count",
+                "time_between_games",
+                "playoff",
+                "win",
+                "winner",
+                "HOME_TEAM_ABBREVIATION",
+                "AWAY_TEAM_ABBREVIATION",
+            ]
+            and col not in percentage_columns)
+        ]
+
+        for col in averaging_columns:
+
+            running_avg_col_name = f"running_avg_{col}_last_{self.span}"
+
+            group[running_avg_col_name] = (
+                group[col].ewm(span=self.span, min_periods=1).mean().shift(self.shift)
+            )
+        group = group.drop(columns=averaging_columns)
+        return group
 
 if __name__ == "__main__":
 
@@ -153,4 +256,13 @@ if __name__ == "__main__":
 
     p50 = Preprocessor(seasons, 50, 1)
     p25 = Preprocessor(seasons, 25, 1)
-    p10 = Preprocessor(seasons, 25, 1)
+    p10 = Preprocessor(seasons, 10, 1)
+
+    common_cols = list(set(p50.team_stats.columns).intersection(set(p25.team_stats.columns)))
+
+    p50.team_stats = pd.merge(p50.team_stats, p25.team_stats, on=common_cols, how="inner")
+    p50.team_stats = pd.merge(p50.team_stats, p10.team_stats, on=common_cols, how="inner")
+
+    print("Processing complete, saving data")
+    p50.games.to_csv("all_games.csv")
+    p50.team_stats.to_csv("all_team_averages.csv")
